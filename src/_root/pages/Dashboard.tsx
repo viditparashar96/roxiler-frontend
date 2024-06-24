4fc3e4ab6352507a23d657d12597241e57add5e5
Vidit Parashar <87743932+viditparashar96@users.noreply.github.com>
2024-06-24 07:56:33 +0530

    Update Dashboard.tsx

--- src/_root/pages/Dashboard.tsx
+++ src/_root/pages/Dashboard.tsx
@@ -133,7 +133,7 @@ const Dashboard = () => {
     <>
       <div className="h-full ">
         <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
-          <div className="flex items-center justify-between space-y-2">
+          <div className=" space-y-3">
             <h2 className="text-3xl font-bold tracking-tight">
               Hi, Welcome back 👋
             </h2>
